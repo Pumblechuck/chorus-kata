1224f0302558dcfea75fb8203392e0c307486f4a
Jack Carson <jack.carson@cdl.co.uk>
2020-06-07 20:02:01 +0100

    Non Functional Dynamic(-ish) Basket

--- chorus-kata/src/Components/Basket.jsx
+++ chorus-kata/src/Components/Basket.jsx
@@ -5,8 +5,26 @@ import Counter from "./Counter";
 class Basket extends Component {
 	render() {
 		return (
-			<div className="basket">
-				<Counter />
+			<div className="card basket">
+				<div className="card-header">
+					<h3 className="card=title">Basket</h3>
+				</div>
+				<div className="card-body">
+					<div className="row row-fluid">
+						<div className="col-12">
+							{this.props.items.map((item) => (
+								<Counter key={item.id} item={item}>
+									<span className="itemID"> // #{item.id}</span>
+								</Counter>
+							))}
+							<div className="row row-fluid">
+								<div className="col-12">
+									<button className="btn btn-warning btn-sm m-1">Reset</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- chorus-kata/src/Components/Body.jsx
+++ chorus-kata/src/Components/Body.jsx
@@ -3,11 +3,49 @@ import React, { Component } from "react";
 import Basket from "./Basket";
 
 class Body extends Component {
+	state = {
+		items: [
+			{
+				id: 1,
+				name: "A",
+				value: 0,
+				price: 0.5,
+				total: 0,
+			},
+			{
+				id: 2,
+				name: "B",
+				value: 0,
+				price: 0.3,
+				total: 0,
+			},
+			{
+				id: 3,
+				name: "C",
+				value: 0,
+				price: 0.2,
+				total: 0,
+			},
+			{
+				id: 4,
+				name: "D",
+				value: 0,
+				price: 0.15,
+				total: 0,
+			},
+		],
+	};
 	render() {
 		return (
-			<React.Fragment>
-				<Basket />
-			</React.Fragment>
+			<div className="col-4 offset-4">
+				<Basket items={this.state.items} />
+				<div className="text-center m-3">
+					<span className="badge badge-secondary badge-pill p-2">
+						Items in basket:
+						<b> {this.state.items.filter((c) => c.value > 0).length}</b>
+					</span>
+				</div>
+			</div>
 		);
 	}
 }
